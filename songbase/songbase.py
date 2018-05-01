8a72b97679dad1498b72b2ffa4109ce88b47d6d5
Judy Yang <judyyang@udel.edu>
2018-05-01 10:36:15 -0400

    flask

--- songbase/songbase.py
+++ songbase/songbase.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-        return "hello world"
+    # return "hello judy yang"
+    return render_template ('index.html')
 
 
 @app.route('/user')
 def user():
     return "this is the page for users"
+
+
+@app.route('/users/<string:username>')
+def users(username):
+    # return "hello %s" %username
+    return render_template('user.html', uname=username)
 
 
 if __name__ =='__main__':
